bc84ccb788cd6220c9dbd3e31cbbdd024a138a07
Bai Dong <bai123donggood@163.com>
2015-08-25 22:05:05 +0800

    add Permissions(not all the permissons, did not finishgit status)

--- app/models.py
+++ app/models.py
@@ -15,7 +15,11 @@ from . import db, login_manager
 
 
 class Permission:
-    pass
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 
 class Role(db.Model):
