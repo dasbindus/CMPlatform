0bf17d97471e42bdcc77cbd510f84acc8f141e04
Bai Dong <bai123donggood@163.com>
2015-08-26 21:03:47 +0800

    Permissions => User roles.

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,3 +5,14 @@ __author__ = 'Jack Bai'
 
 from flask import render_template, request, jsonify
 from . import main
+
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
