ba1f19f54bc091a96ced9f1151a6564f960390a0
Jack Bai <bai123donggood@163.com>
2016-04-10 20:52:10 +0800

    Api PUT /data/int<int:id>

--- app/api_1_0/data.py
+++ app/api_1_0/data.py
@@ -16,6 +16,25 @@ import datetime
 
 
 
+@api.route('/data/', methods=['GET'])
+def get_all_data():
+    page = request.args.get('page', 1, type=int)
+    pagination = CarData.query.paginate(
+        page, per_page=current_app.config['DATA_PER_PAGE'], error_out=False)
+    datas = pagination.items
+    prev = None
+    next = None
+    if pagination.has_prev:
+        prev = url_for('api.get_all_data', page=page-1, _external=True)
+    if pagination.has_next:
+        next = url_for('api.get_all_data', page=page+1, _external=True)
+    return jsonify({
+        'cars': [data.to_json() for data in datas],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total,
+        'time': datetime.datetime.utcnow()
+    })
 
 
 
@@ -23,4 +42,21 @@ import datetime
 def get_data(id):
     data = CarData.query.get_or_404(id)
     return jsonify(data.to_json())
+
+
+@api.route('/data/<int:id>', methods=['PUT'])
+@permission_required(Permission.ADMINISTER)
+def update_data(id):
+    data_exist = CarData.query.get(id)
+    # TODO throw forbidden when not owner or admin
+    if data_exist is None:
+        return not_found('Resources not found. Please POST to create.')
+    data = CarData.from_json(request.json)
+    data.car_id = id
+    db.session.add(data)
+    db.session.commit()
+    return jsonify({
+        'data': data.to_json(),
+        'create_at': datetime.datetime.utcnow()
+    })
 
